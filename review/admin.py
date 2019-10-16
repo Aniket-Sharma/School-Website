ff9422b551a33f8a92ba10b425abbe1100bbe0dc
Aniket Sharma <abcd13017@gmail.com>
2019-10-16 21:54:30 +0530

    changed admin

--- review/admin.py
+++ review/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Review, Teacher, Contact
+from .models import Review, Teacher, Contact, Subscriber
 
 admin.site.register(Teacher)
 admin.site.register(Review)
 admin.site.register(Contact)
+admin.site.register(Subscriber)
